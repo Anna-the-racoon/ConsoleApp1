5e182fb408a1c0793de41e2e2b97ca2faf9aee75
wwww.anechka@gmail.com <wwww.anechka@gmail.com>
2021-10-17 16:38:40 +0300

    Make independence library to work with TimeSpan

--- Program.cs
+++ Program.cs
@@ -1,27 +1,8 @@
 ﻿using System;
+using TimeSpanLlibrary;
 
-namespace ConsoleApp1
+namespace ConsoleResults
 {
-    public struct Result
-    {
-        public static string Convert(DateTime start, DateTime end)
-        {
-            var months = 12 * (end.Year - start.Year) + (end.Month - start.Month);
-
-            var days = end.Day - start.Day;
-
-            var years = months / 12;
-            
-            months -= years * 12;
-
-            var experience = years <= 0 ? null : years + (years == 1 ? " year " : " years ");
-            experience += months <= 0 ? null : months + (months == 1 ? " month " : " months ");
-            experience += days <= 0 ? null : days + (days == 1 ? " day " : " days ");
-
-            return experience;
-        }
-    }
-
     public class Program
     {
         static void Main()
